db6d397bf4a15676df18555ee19faf9b5e47cd4e
mostafa <mostafa@DESKTOP-JPOLLIT>
2022-10-27 23:01:13 +0000

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercice1_2
+{
+    class ProgramPrincipale
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
